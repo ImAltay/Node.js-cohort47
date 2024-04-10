3d739e82f4e92a39df913dd9b5425279f51e1515
ibrahim ihsan şahin <etliekmek97@gmail.com>
2024-04-10 23:56:26 +0200

    key

--- homework/server.js
+++ homework/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fetch from 'node-fetch';
+import { KEYS } from './sources/keys.js';
 
 const app = express();
 
@@ -10,8 +12,15 @@ app.get('/', (req, res) => {
 
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
-
-  res.json(cityName);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${KEYS.API_KEY}`
+  )
+    .then((response) => {
+      return res.json(response);
+    })
+    .catch((error) => {
+      return res.send('Error: ' + error);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ homework/sources/keys.js
@@ -0,0 +1,3 @@
+export const KEYS = {
+  API_KEY: 'ab7a23dee31f12499b975ff6e27c90b0',
+};

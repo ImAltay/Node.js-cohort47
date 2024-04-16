0be13426125ee91b3dd529fa6b99188f34c8a34c
ibrahim ihsan şahin <etliekmek97@gmail.com>
2024-04-16 15:29:12 +0200

    pc swap

--- week3/prep-exercise/app.js
+++ week3/prep-exercise/app.js
@@ -1,22 +1,51 @@
 import express from 'express';
-import { isValidUser, addDB } from './users.js';
+import {
+  isValidUser,
+  getDB,
+  updateDB,
+  hashPassword,
+  generateID,
+  comparePassword,
+} from './users.js';
 import fs from 'fs';
-// Use below import statement for importing middlewares from users.js for your routes
-// import { ....... } from "./users.js";
 
 let app = express();
 
 app.use(express.json());
 // Create routes here, e.g. app.post("/register", .......)
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const newUser = {
+    id: generateID(),
     username: req.body.username,
-    password: req.body.password,
+    password: await hashPassword(req.body.password),
   };
-  isValidUser(newUser);
-  addDB(newUser);
-  res.status(201).send({ username: newUser.username }).end();
+  if (!isValidUser(newUser).isValid) {
+    isValidUser(newUser, res).result();
+  }
+
+  const db = getDB();
+  db.users.push(newUser);
+  updateDB(db);
+
+  res.status(201).send({ id: newUser.id, username: newUser.username }).end();
+});
+
+app.post('/login', async (req, res) => {
+  if (!req.body.password || !req.body.username) {
+    return res.status(401).send('user name or password cant be empty').end();
+  }
+  const db = getDB();
+  const user = db.users.find((user) => user.username === req.body.username);
+
+  if (!user) {
+    return res.status(404).send('USER NOT FOUND').end();
+  }
+  if (await comparePassword(req.body.password, user.password)) {
+    return res.status(200).send('Successful login').end();
+  } else {
+    return res.status(401).send('WRONG PASSWORD').end();
+  }
 });
 
 app.listen(3000, () => {
--- week3/prep-exercise/users.js
+++ week3/prep-exercise/users.js
@@ -1,20 +1,56 @@
-import { userDB, addUser } from './database.js';
+import fs from 'fs';
+import bcrypt from 'bcrypt';
+import { v4 } from 'uuid';
 
 // export const register = async (req, res) => {};
 
-export const isValidUser = (newUser) => {
-  if (!newUser) {
-    res.status(400).send('invalid user').end();
-    return;
+export const isValidUser = (newUser, res) => {
+  if (!newUser.username) {
+    return {
+      isValid: false,
+      result: () => {
+        res.status(400).send('Username can not be empty').end();
+      },
+    };
   }
-  if (newUser.password < 6) {
-    res.status(400).send('password should be at least 6 char').end();
-    return;
+  if (!newUser.password) {
+    return {
+      isValid: false,
+      result: () => {
+        res.status(400).send('password can not be empty').end();
+      },
+    };
   }
-
+  if (newUser.password.length < 6) {
+    return {
+      isValid: false,
+      result: () => {
+        res.status(400).send('password should be at least 6 char').end();
+      },
+    };
+  }
+  return { isValid: true };
   // you can add more checks but whatever
 };
 
-export const addDB = (newUser) => {
-  addUser(newUser);
+export const getDB = () => {
+  // json to object from database.json
+  return JSON.parse(fs.readFileSync('./database.json'));
+};
+
+export const updateDB = (newDB) => {
+  // object to json to database.json
+  fs.writeFileSync('./database.json', JSON.stringify(newDB));
+};
+
+export const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 12);
+};
+
+export const comparePassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+export const generateID = () => {
+  return v4();
 };

bd9d4cad515e44c0b2f3e88da5f1c388286c118f
ibrahim ihsan şahin <etliekmek97@gmail.com>
2024-04-27 09:42:41 +0200

    corrected tests

--- homework/__test__/app.test.js
+++ homework/__test__/app.test.js
@@ -16,23 +16,23 @@ describe('POST /', () => {
 
 describe('POST /weather/:cityName', () => {
   it('Should return a 200 status', async () => {
-    const response = await request.post('/weather/London');
+    const response = await request.get('/weather/London');
     expect(response.status).toBe(200);
   });
 
   it('Should return a 404 status', async () => {
-    const response = await request.post('/weather/NotACity');
+    const response = await request.get('/weather/NotACity');
     expect(response.status).toBe(404);
   });
 
   it('Should return a 200 status with the correct data', async () => {
-    const response = await request.post('/weather/London');
+    const response = await request.get('/weather/London');
     expect(response.body.cityName).toBe('London');
     expect(response.body.temperature).toBeDefined();
   });
 
   it('Should return a 404 status with the correct data', async () => {
-    const response = await request.post('/weather/NotACity');
+    const response = await request.get('/weather/NotACity');
     expect(response.body.weatherText).toBe('City not found!');
   });
 });
